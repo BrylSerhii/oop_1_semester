0b78777b9f2cb681aaaa5b19738e0278a27d2d75
BrylSerhii <iggrooot@gmail.com>
2023-10-03 22:06:23 +0300

    Revert "лабороторна робота 1. Перелік 1(4), перелік 2(1)"
    This reverts commit 740d07304be7e87b9b4af8984cdb06e960e43c7d.

--- Shape.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Lab_1
-{
-    public abstract class Shape
-    {
-        public abstract double Perimeter();
-        public abstract double Area();
-        public abstract bool IsSpecial();
-    }
-}
