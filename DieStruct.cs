91652355fd76df34cbbc48c2dd3401fe8d716b8a
BrylSerhii <iggrooot@gmail.com>
2023-10-05 13:01:12 +0300

    add a die struct class file

---
+++ DieStruct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+struct Die
+{
+    public int Faces { get; }
+    public double[] Probabilities { get; }
+
+    public Die(int faces, double[] probabilities)
+    {
+        Faces = faces;
+        Probabilities = probabilities;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,28 @@
-﻿using System;
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        List<Die> diceSet = new List<Die>
+        {
+            new Die(6, new double[] { 1.0 / 6, 1.0 / 6, 1.0 / 6, 1.0 / 6, 1.0 / 6, 1.0 / 6 }),
+            new Die(4, new double[] { 0.25, 0.25, 0.25, 0.25 })
+        };
 
-Person person = new Person("Tom", 31, "tom@gmail.com");
-person.print();
+        var sumProbabilities = DiceProbabilityCalculator.CalculateSumProbabilities(diceSet);
+
+        Console.WriteLine("All possible sums and their probabilities:");
+        foreach (var entry in sumProbabilities)
+        {
+            Console.WriteLine($"Sum: {entry.Key}, Probability: {entry.Value}");
+        }
+
+        Console.WriteLine("\nSorted sums using Insertion Sort:");
+        SortingAlgorithms.InsertionSort(new Dictionary<int, double>(sumProbabilities));
+
+        Console.WriteLine("\nSorted sums using Quicksort:");
+        SortingAlgorithms.QuickSort(new Dictionary<int, double>(sumProbabilities));
+
+        Console.WriteLine("\nSorted sums using Merge Sort:");
+        SortingAlgorithms.MergeSort(new Dictionary<int, double>(sumProbabilities));
+    }
+}
